146c5b51cc60b1bfd50cf03f8474646dfe81172f
Will Koehrsen <wjk68@case.edu>
2017-04-21 10:04:25 -0500

    Finished evaluation/validation

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -13,9 +13,6 @@ The dataset used in this example is a preprocessed excerpt of the
   original source: http://scikit-learn.org/stable/auto_examples/applications/face_recognition.html
 
 """
-
-
-
 print __doc__
 
 from time import time
@@ -23,12 +20,12 @@ import logging
 import pylab as pl
 import numpy as np
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_lfw_people
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-from sklearn.decomposition import RandomizedPCA
+from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 
 # Display progress logs on stdout
@@ -66,54 +63,54 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+def classify(n_components):
 
-print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
-t0 = time()
-pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
-print "done in %0.3fs" % (time() - t0)
+    print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
+    t0 = time()
+    pca = PCA(svd_solver='randomized', n_components=n_components, whiten=True).fit(X_train)
+    print "done in %0.3fs" % (time() - t0)
 
-eigenfaces = pca.components_.reshape((n_components, h, w))
+    eigenfaces = pca.components_.reshape((n_components, h, w))
 
-print "Projecting the input data on the eigenfaces orthonormal basis"
-t0 = time()
-X_train_pca = pca.transform(X_train)
-X_test_pca = pca.transform(X_test)
-print "done in %0.3fs" % (time() - t0)
-
-
-###############################################################################
-# Train a SVM classification model
-
-print "Fitting the classifier to the training set"
-t0 = time()
-param_grid = {
-         'C': [1e3, 5e3, 1e4, 5e4, 1e5],
-          'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
-          }
-# for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
-clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
-clf = clf.fit(X_train_pca, y_train)
-print "done in %0.3fs" % (time() - t0)
-print "Best estimator found by grid search:"
-print clf.best_estimator_
+    print "Projecting the input data on the eigenfaces orthonormal basis"
+    t0 = time()
+    X_train_pca = pca.transform(X_train)
+    X_test_pca = pca.transform(X_test)
+    print "done in %0.3fs" % (time() - t0)
 
 
-###############################################################################
-# Quantitative evaluation of the model quality on the test set
+    ###############################################################################
+    # Train a SVM classification model
 
-print "Predicting the people names on the testing set"
-t0 = time()
-y_pred = clf.predict(X_test_pca)
-print "done in %0.3fs" % (time() - t0)
+    print "Fitting the classifier to the training set"
+    t0 = time()
+    param_grid = {
+             'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
+              }
+    # for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
+    clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+    clf = clf.fit(X_train_pca, y_train)
+    print "done in %0.3fs" % (time() - t0)
+    print "Best estimator found by grid search:"
+    print clf.best_estimator_
 
-print classification_report(y_test, y_pred, target_names=target_names)
-print confusion_matrix(y_test, y_pred, labels=range(n_classes))
 
+    ###############################################################################
+    # Quantitative evaluation of the model quality on the test set
+
+    print "Predicting the people names on the testing set"
+    t0 = time()
+    y_pred = clf.predict(X_test_pca)
+    print "done in %0.3fs" % (time() - t0)
+
+    print classification_report(y_test, y_pred, target_names=target_names)
+    print confusion_matrix(y_test, y_pred, labels=range(n_classes))
+    return pca, clf
 
 ###############################################################################
 # Qualitative evaluation of the predictions using matplotlib
-
+'''
 def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
     """Helper function to plot a gallery of portraits"""
     pl.figure(figsize=(1.8 * n_col, 2.4 * n_row))
@@ -144,3 +141,9 @@ eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
 plot_gallery(eigenfaces, eigenface_titles, h, w)
 
 pl.show()
+'''
+
+if __name__=="__main__":
+    n_components_to_try = [10, 15, 25, 50, 100, 250]
+    for n_components in n_components_to_try:
+        classify(n_components)
