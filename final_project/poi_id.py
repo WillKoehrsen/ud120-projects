ca26a20817c143a5045db79968016bdb1b537ebf
Will Koehrsen <wjk68@case.edu>
2017-04-23 19:04:09 -0600

    Working on P5, starting to get going

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -6,18 +6,55 @@ sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
-
-### Task 1: Select what features you'll use.
-### features_list is a list of strings, each of which is a feature name.
-### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import train_test_split
+import numpy as np
+import pandas as pd
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-### Task 2: Remove outliers
-### Task 3: Create new feature(s)
+# Create a dataframe from the dictionary
+df = pd.DataFrame(data_dict)
+
+# Transpose so the rows are individuals and columns are data fields
+df = df.transpose()
+df.drop(['TOTAL'], inplace=True)
+
+# Create a list of all the data fields
+fields = df.columns.tolist()
+# All data fields are numeric except email_address and poi
+fields.remove('email_address')
+fields.remove('poi')
+
+# Iterate through all the columns and convert data to floating point
+for field in fields:
+	try:
+		df[field] = df[field].astype(float)
+	except Exception as e:
+		print e
+
+# Create new features
+# First feature is ratio of emails recieved that are from a person of interest
+df['from_ratio'] = df['from_poi_to_this_person'] / df['to_messages']
+# Second new feature is ratio of emails sent that are to a person of interest
+df['to_ratio'] = df['from_this_person_to_poi'] / df['from_messages']
+# Third new feature is ratio of emails received that shared a receipt with a person of interest
+df['shared_ratio'] = df['shared_receipt_with_poi'] / df['to_messages']
+
+# Drop all NaNs from data
+df = df.dropna(subset=['to_ratio', 'from_ratio', 'shared_ratio'])
+
+# Transpose back and convert to dictionary where each key is the individual and values are dictionaries of the data
+data_dict = df.transpose().to_dict()
+
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+features_list = ['poi', 'to_ratio', 'from_ratio'] 
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
@@ -25,15 +62,17 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.25)
 ### Task 4: Try a variety of classifiers
 ### Please name your classifier clf for easy export below.
 ### Note that if you want to do PCA or other multi-stage operations,
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+
+
+clf = DecisionTreeClassifier()
+
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -43,7 +82,7 @@ clf = GaussianNB()
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
--- final_project/tester.py
+++ final_project/tester.py
@@ -25,12 +25,12 @@ RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFals
 def test_classifier(clf, dataset, feature_list, folds = 1000):
     data = featureFormat(dataset, feature_list, sort_keys = True)
     labels, features = targetFeatureSplit(data)
-    cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
+    cv = StratifiedShuffleSplit(random_state = 42)
     true_negatives = 0
     false_negatives = 0
     true_positives = 0
     false_positives = 0
-    for train_idx, test_idx in cv: 
+    for train_idx, test_idx in cv.split(features, labels): 
         features_train = []
         features_test  = []
         labels_train   = []
@@ -74,6 +74,8 @@ def test_classifier(clf, dataset, feature_list, folds = 1000):
         print "Got a divide by zero when trying out:", clf
         print "Precision or recall may be undefined due to a lack of true positive predicitons."
 
+    return clf
+
 CLF_PICKLE_FILENAME = "my_classifier.pkl"
 DATASET_PICKLE_FILENAME = "my_dataset.pkl"
 FEATURE_LIST_FILENAME = "my_feature_list.pkl"
@@ -99,7 +101,9 @@ def main():
     ### load up student's classifier, dataset, and feature_list
     clf, dataset, feature_list = load_classifier_and_data()
     ### Run testing script
-    test_classifier(clf, dataset, feature_list)
-
+    clf = test_classifier(clf, dataset, feature_list)
+    return clf
+    
 if __name__ == '__main__':
-    main()
+    clf = main()
+    print clf.feature_importances_
